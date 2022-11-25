3d63ae8c0fcc99302437403bb94295e108876a5c
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-11-24 23:43:28 -0500

    rename file and root id

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import { render } from 'preact'
 import { App } from './App'
 import './style.css'
 
-render(<App />, document.querySelector('#app')!)
+render(<App />, document.getElementById('root')!)
